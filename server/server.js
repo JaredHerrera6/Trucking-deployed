9ecf4c2b7b3a24fe59ff7702056797326cd272e3
JaredHerrera6 <herrerajared13@gmail.com>
2023-11-06 19:17:16 -0500

    rearranging files

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express")
 const app = express();
-const db = require("./db") 
+const db = require(".") 
 const cors = require("cors")
 
 app.use(cors());

14ecdec6f8757a4b64614d486e48e661f7fa394c
JaredHerrera6 <herrerajared13@gmail.com>
2023-11-07 15:58:40 -0500

    db to index

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require('express')
 const app = express();
-const db = require("db/db") 
+const db = require("./index") 
 const cors = require("cors")
 
 app.use(cors());
